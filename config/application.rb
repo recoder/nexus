9beec76e0e13c4df21b6e8189f51a6554d4fe06f
Andrew Kondrashov <recoder@gmail.com>
2018-06-08 15:35:16 +0000

    Introduce `app/lib` diirectory

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,12 @@ module Nexvs
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
+
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Auto-load library classes
+    config.autoload_paths += %W(#{config.root}/app/lib)
   end
 end
